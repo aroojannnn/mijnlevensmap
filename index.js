3df9c4031223a4bf8e067bbb7384c7b1407eb7f8
aroojannnn <aroo@veldhuisbouw.nl>
2026-05-27 09:34:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,55 +11,47 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+
+// Hiermee kan Render style.css uit de hoofdmap laden
+app.use(express.static(__dirname));
+
+function sendHtml(res, fileName) {
+  const filePath = path.join(__dirname, fileName);
+  const html = fs.readFileSync(filePath, 'utf8');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.send(html);
+}
 
 app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'home.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
+  sendHtml(res, 'home.html');
 });
+
 app.get('/over-ons', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'over-ons.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
+  sendHtml(res, 'over-ons.html');
 });
-app.get('/veelgestelde-vragen', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'veelgestelde-vragen.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
-});
-app.get('/privacy', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'privacy.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
-});
+
 app.get('/hoe-werkt-het', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'hoe-werkt-het.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
+  sendHtml(res, 'hoe-werkt-het.html');
 });
+
+app.get('/veelgestelde-vragen', (req, res) => {
+  sendHtml(res, 'veelgestelde-vragen.html');
+});
+
+app.get('/privacy', (req, res) => {
+  sendHtml(res, 'privacy.html');
+});
+
 app.get('/login', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'login.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
+  sendHtml(res, 'login.html');
 });
+
 app.get('/dashboard', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'dashboard.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
+  sendHtml(res, 'dashboard.html');
 });
+
 app.get('/contact', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'contact.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
+  sendHtml(res, 'contact.html');
 });
 
 app.get('/api/status', (req, res) => {
